8d72bbd63afd72feb954cbe81f85c93854dc6bca
Svend <svc001@edu.zealand.dk>
2025-02-12 12:10:47 +0100

    Landing page delvist færdig ud fra figma

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ require "settings/init.php";
 <head>
     <meta charset="utf-8">
     
-    <title>Sigende titel</title>
+    <title>Landing Page</title>
     
     <meta name="robots" content="All">
     <meta name="author" content="Udgiver">
@@ -23,9 +23,30 @@ require "settings/init.php";
 
 <body>
 
-<h1>Det virker 🥳</h1>
-<p>dette er en test 3</p>
-<p>Din mor</p>
+<nav class="bg-black d-flex justify-content-between p-3 pb-5">
+    <a href="#"><img src="images/whitelogo.png" alt="Logo" width="179" height="75"></a>
+    <div class="d-flex justify-content-between align-items-center w-25">
+        <a href="#" class="text-white">Log ind</a>
+        <a href="registrer.php" class="rounded-4 bg-primary p-2 pe-3 ps-3 text-white">Tilmeld</a>
+    </div>
+</nav>
+
+<div class="bg-black container-fluid">
+    <div class="row text-center pt-5 pb-5">
+        <h1 class="text-white">Gør dine bookinger og aktivitet registreringer lette<br>og kom i gang med det samme!</h1>
+        <p class="text-white">QUICK DRAW hjælper dine medlemmer for nem og hurtig registrering af aktivitet<br>ved bare et simpelt tryk</p>
+    </div>
+
+    <div class="row mt-5 justify-content-center pb-5">
+        <div class="col-2 text-center">
+            <a href="#" class="bg-primary p-2 pe-3 ps-3 text-white rounded-2 fs-3">Tilmeld dig nu!</a>
+        </div>
+
+        <div class="col-2 text-center">
+            <a href="#" class="text-white fs-3  ">Find bookninger</a>
+        </div>
+    </div>
+</div>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
