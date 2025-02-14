a0bc01466b372895453cb84edde1aac114ecbce2
willi <williamhansen843@gmail.com>
2025-02-14 10:48:05 +0100

    footer og en farve til styles

--- support.php
+++ support.php
@@ -23,9 +23,43 @@ require "settings/init.php";
 <div class="text-center bg-qBlaa pb-3 pt-3">
     <img src="images/whitelogo.png" class="img-fluid" alt="logo">
 </div>
+
+
 <body>
+
+
+<div>
+    <p> halajsa</p>
+    <p> halajsa</p>
+    <p> halajsa</p>
+    <p> halajsa</p>
+    <p> halajsa</p>
+</div>
+
+<footer>
+    <nav class="navbar">
+        <div class="container-fluid bg-qBlaa">
+            <div class="collapse navbar-collapse d-flex justify-content-center" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active text-white" aria-current="page" href="#">Inbox</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="#">Booking</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="#">Support</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+</footer>
+
 
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
+
 </html>
