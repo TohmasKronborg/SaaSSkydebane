571209151d3392f9f536960a276ae7e499a6cd7c
NoahVN <non002@edu.zealand.dk>
2025-02-17 17:30:32 +0100

    Revert "Initial version of registrer.php"
    This reverts commit 06362095b232f5e882857431901ec5807d45b8bc.

--- registrer.php
+++ registrer.php
@@ -23,33 +23,9 @@ require "settings/init.php";
 
 <body>
 
-<div class="text-center bg-qBlaa ps-5 pe-5 pt-3 pb-3">
-    <img src="images/whitelogo.png" class="img-fluid" alt="logo">
-</div>
-
-<div class="container">
-    <div class="row">
-        <div class="col d-flex justify-content-center m-3">
-            <p class="h2">Registrér</p>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col d-flex justify-content-center m-3 bg-qGraa p-2 flex-column">
-            <p class="h4 d-flex justify-content-center">E-mail</p>
-            <form>
-                <div class="mb-3">
-                    <label for="registerEmail" class="form-label">E-mail adresse</label>
-                    <input type="email" class="form-control shadow-sm" id="registerEmail" aria-describedby="emailHelp">
-                </div>
-                <div class="mb-3">
-                    <label for="registerPassword" class="form-label">Password</label>
-                    <input type="password" class="form-control shadow-sm" id="registerPassword">
-                </div>
-                <button type="submit" class="btn btn-qRod text-white shadow-sm">Registrér</button>
-            </form>
-        </div>
-    </div>
-</div>
+<h1>Det virker 🥳</h1>
+<p>dette er en test 3</p>
+<p>Din mor</p>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
