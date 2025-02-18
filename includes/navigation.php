b496d1cfd3bcada0b64d154d8af25e859af902ce
Svend <svc001@edu.zealand.dk>
2025-02-18 10:44:12 +0100

    Fixed href linkse i include

--- includes/navigation.php
+++ includes/navigation.php
@@ -1,9 +1,9 @@
 <footer class="mt-auto">
     <div class="bg-qBlaa d-flex justify-content-evenly">
-        <a class="nav-link active text-white" aria-current="page" href="#">Inbox</a>
+        <a class="nav-link active text-white" aria-current="page" href="../forside.php">Inbox</a>
         <span class="text-white">|</span>
-        <a class="nav-link text-white" href="#">Booking</a>
+        <a class="nav-link text-white" href="../booking.php">Booking</a>
         <span class="text-white">|</span>
-        <a class="nav-link text-white" href="#">Support</a>
+        <a class="nav-link text-white" href="../support.php">Support</a>
     </div>
 </footer>
