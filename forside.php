56b51dc6444f1eaaf8094d0d80ef1ef12229463d
Svend <svc001@edu.zealand.dk>
2025-02-19 16:00:50 +0100

    Php tilføjet på forsiden så det er nu muligt at se møder

--- forside.php
+++ forside.php
@@ -44,9 +44,41 @@ require "settings/init.php";
 include("includes/logoHeader.php");
 ?>
 
-<h1 class="fw-bold d-flex justify-content-center pt-4 pb-2 ">Dine tider</h1>
+<h1 class="fw-bold d-flex justify-content-center pt-4 pb-2 ">Bookede tider</h1>
 <!-- Box til announcements -->
+<div class="bookingBox overflow-y-scroll rounded-4 m-2 border border-5 border-qBlaa shadow">
+    <?php
+    $sqlAdd = "";
+    $bind = [];
 
+    if (!empty($_GET["reservation_id"])) {
+        $sqlAdd = "WHERE r.reservation_id = :reservationId";
+        $bind["reservationId"] = $_GET["reservation_id"];
+    }
+
+    $sql = "SELECT r.reservation_id, r.user_name, l.lane_name, f.firearm_name, r.reservation_time 
+        FROM reservations r
+        JOIN lanes l ON r.lane_id = l.lane_id
+        JOIN firearms f ON r.firearm_id = f.firearm_id
+        " . $sqlAdd;
+
+    $meetings = $db->sql($sql, $bind);
+
+    foreach ($meetings as $meeting) {
+        ?>
+        <div class="border border-2 border-qGraa2 rounded-1 p-3 m-2">
+            <?php
+            echo "<p class='m-0'><strong>Starter kl. </strong>" . date("H:i", strtotime($meeting->reservation_time)) ."<strong> d. </strong>" . date("d-m-y", strtotime($meeting->reservation_time)) . "</p>";
+            echo "<p class='m-0'><strong>Bane: </strong>" . htmlspecialchars($meeting->lane_name) . "</p>";
+            echo "<p class='m-0'><strong>Bruger: </strong>" . htmlspecialchars($meeting->user_name) . "</p>";
+            echo "<p class='m-0'><strong>Våben: </strong>" . htmlspecialchars($meeting->firearm_name) . "</p>";
+            ?>
+        </div>
+        <?php
+    }
+
+    ?>
+</div>
 
 
 <h1 class="fw-bold d-flex justify-content-center pt-4 pb-2 ">Nyheder</h1>
