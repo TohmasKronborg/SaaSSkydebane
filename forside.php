9a1ebff434f1545229c0dc7e0698d6125d1140e3
Svend <svc001@edu.zealand.dk>
2025-02-19 15:03:13 +0100

    Klar til at lave ændringer på forsiden

--- forside.php
+++ forside.php
@@ -27,65 +27,33 @@ require "settings/init.php";
     </style>
 
 </head>
-
+<!--
+.▄▄▄  ▄• ▄▌▪   ▄▄· ▄ •▄
+▐▀•▀█ █▪██▌██ ▐█ ▌▪█▌▄▌▪
+█▌·.█▌█▌▐█▌▐█·██ ▄▄▐▀▀▄·
+▐█▪▄█·▐█▄█▌▐█▌▐███▌▐█.█▌
+·▀▀█.  ▀▀▀ ▀▀▀·▀▀▀ ·▀  ▀
+·▄▄▄▄  ▄▄▄   ▄▄▄· ▄▄▌ ▐ ▄▌
+██▪ ██ ▀▄ █·▐█ ▀█ ██· █▌▐█
+▐█· ▐█▌▐▀▀▄ ▄█▀▀█ ██▪▐█▐▐▌
+██. ██ ▐█•█▌▐█ ▪▐▌▐█▌██▐█▌
+▀▀▀▀▀• .▀  ▀ ▀  ▀  ▀▀▀▀ ▀▪
+-->
+<body>
 <?php
 include("includes/logoHeader.php");
 ?>
 
+<h1 class="fw-bold d-flex justify-content-center pt-4 pb-2 ">Dine tider</h1>
+<!-- Box til announcements -->
 
-<body>
 
-<h1 class="fw-bold d-flex justify-content-center pt-4 pb-2 "> Dine tider</h1>
 
-<!-- dette er carrusellen til dine bookede tider -->
+<h1 class="fw-bold d-flex justify-content-center pt-4 pb-2 ">Nyheder</h1>
 
-<div id="carouselExampleDark" class="carousel carousel-dark slide">
-    <div class="carousel-indicators">
-        <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-        <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
-        <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
-    </div>
-    <div class="carousel-inner">
-        <div class="carousel-item active" data-bs-interval="10000">
-            <div class="carousel-caption">
-                <h1>Din Mor</h1>
-            </div>
-            <h5>Februar</h5>
-            <p>Dato: Torsdag 20/2/2025</p>
-            <p>Bane: 3</p>
-            <p>Skydevåben: <br> Grovpistol</p>
-            <span>Tidspunkt:<br> <span class="fw-light">17.00-18.00</span></span>
-        </div>
-        <div class="carousel-item" data-bs-interval="2000">
-            <img src="images/graato.png" class="d-block w-100" alt="">
-            <div class="carousel-caption">
-                <h5>Marts</h5>
-                <p>Dato: Torsdag 06/3/2025</p>
-                <p>Bane: 2</p>
-                <p>Skydevåben: <br> Salonpistol</p>
-                <span>Tidspunkt:<br> <span class="fw-light">17.00-18.00</span></span>
-            </div>
-        </div>
-        <div class="carousel-item">
-            <img src="images/graato.png" class="d-block w-100" alt="background">
-            <div class="carousel-caption">
-                <h5>April</h5>
-                <p>Dato: Fredag 04/4/2025</p>
-                <p>Bane: 4</p>
-                <p>Skydevåben:<br> Salonriffel</p>
-                <span>Tidspunkt:<br> <span class="fw-light">18.30-19.30</span></span>
-            </div>
-        </div>
-    </div>
-    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Previous</span>
-    </button>
-    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Next</span>
-    </button>
-</div>
+
+
+
 
 
 
@@ -97,5 +65,4 @@ include("includes/navigation.php");
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
-
 </html>
